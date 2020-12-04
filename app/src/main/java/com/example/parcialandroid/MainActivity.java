fb53df3fb018f93e5205e22d84380003c40b4df1
juan camilo bermeo <camilo22601@gmail.com>
2020-12-04 17:04:02 -0500

    comentado

--- app/src/main/java/com/example/parcialandroid/MainActivity.java
+++ app/src/main/java/com/example/parcialandroid/MainActivity.java
@@ -50,8 +50,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    private void loadDatabase() {
 
+    private void loadDatabase() {
+        //para cargar la seccion que debe recibir de la base de datos
         DatabaseReference ref = db.getReference().child("preguntas").child("vista");
         ref.addValueEventListener(
                 new ValueEventListener() {
@@ -81,17 +82,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()){
             case R.id.okBtn:
                 Log.e(">>>", "works");
+                //para llamar la seccion que la base de datos debe leer
                 DatabaseReference reference = db.getReference().child("preguntas").child("vista").child(question.getId());
+                //para que recoja el valor que tiene el TextView "espacio"
                 idString = espacio.getText().toString();
+                //para que me convierta en flotante lo que se agrega al EditText "puntaje"
                 float suma = (float) Integer.parseInt(puntaje.getText().toString());
+                //para asignarle un nuevo valor a resultado
                 resultado = resultado + suma;
+                //para sacar el promedio
                 prom = resultado / conteo;
+                //la intencion era sacar asignarle =0 a "conteo" para que no se dañe el promedio
                 if(idString == question.getPregunta()){
                     conteo += 1;
                 }else{
                     conteo = 0;
                 }
 
+                //para convertir en String el resultado de "prom"
                 String nuevoProm = String.valueOf(prom);
 
                 Pregunta quest = new Pregunta(
@@ -102,16 +110,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 );
 
-                if (suma > 10){
+                //las condiciones para que se modifiquen los valores en la base de datos
+                if (suma > 10 /*|| puntaje.getText().toString() == ""*/){
                     Toast.makeText(this, "Debe ingresar una respuesta válida", Toast.LENGTH_SHORT).show();
                 }else {
                     reference.setValue(quest);
                     puntaje.setText("");
 
                 }
-
-                Log.e("conteo" , ""+ conteo);
-                Log.e("idString", idString);
 
                 break;
         }
